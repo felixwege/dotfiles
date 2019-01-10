f8df7937a297753a18e9c2843b5a110cded15c07
Felix Wege <felix.wege@tuhh.de>
2019-01-10 09:16:50 +0100

    unify -w/--window option for gamecon/teamcom

--- .aliases/hulks.sh
+++ .aliases/hulks.sh
@@ -3,7 +3,7 @@
 # start SimRobot, GameController, TeamCommunicationMonitor or mate
 alias simrobot="$HOME/Repositories/HULKs/nao/tools/SimRobot/build/SimRobot"
 alias gamecontroller="cd $HOME/Repositories/HULKs/GameController/build/jar/; java -jar GameController.jar --window &; popd"
-alias teamcommunicationmonitor="cd $HOME/Repositories/HULKs/GameController/build/jar/; java -jar TeamCommunicationMonitor.jar -w &; popd"
+alias teamcommunicationmonitor="cd $HOME/Repositories/HULKs/GameController/build/jar/; java -jar TeamCommunicationMonitor.jar --window &; popd"
 alias mate="~/.venv/nao/bin/python ~/Repositories/HULKs/nao/tools/mate/run.py"
 
 # official robocup spl rules

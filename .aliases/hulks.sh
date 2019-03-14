389a0df98c87c995a63648468d444327896423ae
Felix Wege <felix.wege@tuhh.de>
2019-03-14 19:24:47 +0100

    zshrc: Add game type to gamecontroller alias

--- .aliases/hulks.sh
+++ .aliases/hulks.sh
@@ -2,7 +2,7 @@
 
 # start SimRobot, GameController, TeamCommunicationMonitor or mate
 alias simrobot="$HOME/Repositories/HULKs/nao/tools/SimRobot/build/SimRobot"
-alias gamecontroller="cd $HOME/Repositories/HULKs/GameController/build/jar/; java -jar GameController.jar --window &; popd"
+alias gamecontroller="cd $HOME/Repositories/HULKs/GameController/build/jar/; java -jar GameController.jar --window --game-type preliminary &; popd"
 alias teamcommunicationmonitor="cd $HOME/Repositories/HULKs/GameController/build/jar/; java -jar TeamCommunicationMonitor.jar --window &; popd"
 alias mate="~/.venv/nao/bin/python ~/Repositories/HULKs/nao/tools/mate/run.py"
 

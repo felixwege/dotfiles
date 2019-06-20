23d779bcdc5bd691589d146d35d4dfe0be8eeb7d
Felix Wege <felix.wege@tuhh.de>
2019-06-20 19:26:18 +0200

    zshrc: Pipe mate output to log file

--- .aliases/hulks.sh
+++ .aliases/hulks.sh
@@ -4,7 +4,7 @@
 alias simrobot="$HOME/Repositories/HULKs/nao/tools/SimRobot/build/SimRobot"
 alias gamecontroller="cd $HOME/Repositories/HULKs/GameController/build/jar/; java -jar GameController.jar --window --game-type preliminary &; popd"
 alias teamcommunicationmonitor="cd $HOME/Repositories/HULKs/GameController/build/jar/; java -jar TeamCommunicationMonitor.jar --window &; popd"
-alias mate="~/.venv/nao/bin/python ~/Repositories/HULKs/nao/tools/mate/run.py"
+alias mate="~/.venv/nao/bin/python ~/Repositories/HULKs/nao/tools/mate/run.py | tee -a /tmp/mate.log"
 
 # official robocup spl rules
 alias rules="pdf ~/Documents/HULKs/Rules2019.pdf"

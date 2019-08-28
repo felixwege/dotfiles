dbc104c3cfa36c16063eaa815df5d89c136c38f4
Felix Wege <felix.wege@tuhh.de>
2019-08-28 10:26:13 +0200

    polybar: Launch it on all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,11 @@ killall -q polybar
 # Wait until bars have been terminated
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar
-polybar --reload -c ~/.config/polybar/config.ini main &
+# Launch Polybar on all monitors (https://github.com/polybar/polybar/issues/763)
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini main &
+  done
+else
+  polybar --reload -c ~/.config/polybar/config.ini main &
+fi

75290d9c2a636108a22c4354e210bb24f6d6153c
Felix Wege <felix.wege@tuhh.de>
2020-09-05 20:06:02 +0200

    zshrc: Change timew aliases

--- .aliases/taskwarrior.sh
+++ .aliases/taskwarrior.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 alias t="task"
-alias tt="clear && task"
 alias ta="task add"
-alias td="task done"
-alias te="task edit"
 alias ts="task start"
-alias tm="task modify"
-alias tw="task waiting"
+alias td="task done"
+
+alias tw="timew"
+alias tws="timew start"
+alias twp="timew stop"
+alias tday="timew day"
+alias tweek="timew week"
+alias tmon="timew month"
+alias tsum="timew summary"
